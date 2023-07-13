4f5db864addaf6b060b23b5d08de6e3cbb919041
root <marista032@gmail.com>
2023-07-13 20:04:03 +0000

    main.py fie for task 0

---
+++ 0x02-minimum_operations/0-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the main file that can be used to test the code
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
