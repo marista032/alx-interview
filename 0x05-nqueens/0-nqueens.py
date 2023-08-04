a82b3e8adcc61f08aee7d07ccb007c6c990ca492
marista032 <marista032@gmail.com>
2023-08-03 17:28:21 -0700

    task 0 - solves the N queens problem by backtracking

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+""" this script is based on the N queens puzzle"""
+import sys
+
+
+def isSafe(board, row, col, n):
+    """Cross-check if a queen can be placed on board[row][col]"""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+
+    i, j = row, col
+    while i < n and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i += 1
+        j -= 1
+
+    return True
+
+
+def solveNQUtil(board, col, n, solutions):
+    """ This is a recursive utility function is used to solve the N Queen problem"""
+    if col == n:
+        solution = []
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return True
+
+    res = False
+    for i in range(n):
+        if isSafe(board, i, col, n):
+            board[i][col] = 1
+            res = solveNQUtil(board, col + 1, n, solutions) or res
+            board[i][col] = 0  # BACKTRACK
+
+    return res
+
+
+def solveNQ(n):
+    """This function solves the N Queen problem using Backtracking"""
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
+    solveNQUtil(board, 0, n, solutions)
+
+    # Sort the solutions before printing them
+    solutions.sort()
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solveNQ(n)
